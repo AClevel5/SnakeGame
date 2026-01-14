37cf62cebc0ec70772bebd0c080d79dd7a40568f
alexcleveland <alexcleveland5@gmail.com>
2026-01-13 17:29:53 -0700

    Build Starter Snake

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import turtle
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+x = 0
+y = 0
 
+def grow_snake(x_cord, y_cord):
+    t = turtle.Turtle()
+    t.color("white")
+    t.shape("square")
+    t.goto(x_cord, y_cord)
 
-
-
-
-
+for a in range(3):
+    grow_snake(x, y)
+    x -= 20
 
 
 
